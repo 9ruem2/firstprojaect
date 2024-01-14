4d3a792e282b95cf5d819e19fe7e8b929ce0a867
구름 <118735475+9ruem2@users.noreply.github.com>
2024-01-14 19:13:38 +0900

    1. update, delete 2. delete후, 삭제메세지 창 addFlashAttribute기능

--- src/main/java/com/example/firstprojaect/controller/ArticleController.java
+++ src/main/java/com/example/firstprojaect/controller/ArticleController.java
@@ -7,9 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -45,5 +44,36 @@ public class ArticleController {
         ArrayList<Article> articleList = articleRepository.findAll();
         model.addAttribute("articleList", articleList);
         return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id,Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article",articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("articles/update")
+    public String update(ArticleForm form){
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/"+articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다!");
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+        return "redirect:/articles";
     }
 }
--- src/main/java/com/example/firstprojaect/controller/MemberController.java
+++ src/main/java/com/example/firstprojaect/controller/MemberController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -29,7 +31,7 @@ public class MemberController {
     public String createUser(MemberForm memberForm){
         Member member = memberForm.toEntity();
         Member savedMember = memberRepository.save(member);
-        log.info(savedMember.toString());
+//        log.info(savedMember.toString());
         return "redirect:/members/"+savedMember.getId();
     }
 
@@ -45,6 +47,35 @@ public class MemberController {
         ArrayList<Member> memberList = memberRepository.findAll();
         model.addAttribute("memberList", memberList);
         return "members/index";
+    }
+
+    @GetMapping("members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/edit";
+    }
+
+    @PostMapping("members/update")
+    public String update(MemberForm form){
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        log.info(target.toString());
+        if(target != null){
+            memberRepository.save(memberEntity);
+        }
+        return "redirect:/members/"+memberEntity.getId();
+    }
+
+    @GetMapping("members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        Member target = memberRepository.findById(id).orElse(null);
+        if(target!=null){
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제됐습니다!");
+        }
+        return "redirect:/members";
     }
 
 }
