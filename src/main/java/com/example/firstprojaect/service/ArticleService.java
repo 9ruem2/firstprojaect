0342507739f3e603d779c8efaa328cce375de563
구름 <118735475+9ruem2@users.noreply.github.com>
2024-01-21 18:38:41 +0900

    Junit으로 테스트코드 작성하기 CRUD

--- src/main/java/com/example/firstprojaect/service/ArticleService.java
+++ src/main/java/com/example/firstprojaect/service/ArticleService.java
@@ -5,13 +5,10 @@ import com.example.firstprojaect.entity.Article;
 import com.example.firstprojaect.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
